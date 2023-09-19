d37e5d65ab3c9ff74ddcc58671389fa52c6fe8b9
Irina Zhirenbaeva <irinazirenbaeva@gmail.com>
2023-09-20 00:22:01 +0500

    Task0

---
+++ Tyuiu.ZhirenbaevaII.Sprint1.Task0.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhirenbaevaII.Sprint1.Task0.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhirenbaevaII.Sprint1.Task0.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12488d0e-c460-49d8-8275-5e6987624868")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
