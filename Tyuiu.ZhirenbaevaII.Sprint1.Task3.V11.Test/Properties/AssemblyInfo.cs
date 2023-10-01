5bc0bb78a2316834186b28c3586e103001bf38ac
Irina Zhirenbaeva <irinazirenbaeva@gmail.com>
2023-10-01 17:00:29 +0500

    Task3

---
+++ Tyuiu.ZhirenbaevaII.Sprint1.Task3.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhirenbaevaII.Sprint1.Task3.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhirenbaevaII.Sprint1.Task3.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3a659ba5-fcf6-485c-bf85-a2d4a89696f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZhirenbaevaII.Sprint1.Task3.V11/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZhirenbaevaII.Sprint1.Task3.V11.Lib;
+
+namespace Tyuiu.ZhirenbaevaII.Sprint1.Task3.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Жиренбаева И.И | ИСТНб-23-1";
+
+            Console.WriteLine("");
+            Console.WriteLine(" Спринт #1                                                               ");
+            Console.WriteLine(" Тема: Операторы составного присваивания                                       ");
+            Console.WriteLine(" Задание #3                                                              ");
+            Console.WriteLine(" Вариант #11                                                             ");
+            Console.WriteLine(" Выполнила: Жиренбаева Ирина Ильгизовна | ИСТНб-23-1                       ");
+            Console.WriteLine("");
+            Console.WriteLine("* УСЛОВИЕ:                                                                ");
+            Console.WriteLine(" Написать программу, которая запрашивает у пользователя исходные данные, выполняет указанные расчёты ");
+            Console.WriteLine(" и печатает результат на экране.                                         ");
+            Console.WriteLine("                                                                         ");
+            Console.WriteLine("");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       ");
+            Console.WriteLine("");
+            double x1 = -2;
+            double y1 = 5;
+            double x2 = 1;
+            double y2 = 7;
+            double x3 = 5;
+            double y3 = -3;
+            Console.WriteLine("                                                              ");
+            Console.WriteLine("**");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("**");
+
+            Console.WriteLine(ds.TriangleArea(x1, x2, x3, y1, y2, y3 ));
+
+            Console.ReadLine();
+        }
+    }
+
+}
+    
+
